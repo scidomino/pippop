5aace15bb30153e9f0da137dfc340ab2dc2c49ad
Tommaso Sciortino <sciortino@gmail.com>
2018-10-21 12:04:11 -0700

    Make backwards compatible java 1.7

--- app/src/main/java/com/pippop/MainActivity.java
+++ app/src/main/java/com/pippop/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -21,7 +20,7 @@ public class MainActivity extends Activity {
     TextView showHigh = findViewById(R.id.showHigh);
     showHigh.setText(getBaseContext().getString(R.string.high_score, highscore));
 
-    Button play = findViewById(R.id.play);
+    TextView play = findViewById(R.id.play);
     play.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale));
   }
 
