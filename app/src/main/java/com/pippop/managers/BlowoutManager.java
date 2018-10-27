b385eba9e48a4a2a01ff6a06b50173112b137595
Tommaso Sciortino <sciortino@gmail.com>
2018-10-27 11:23:20 -0700

    refactor

--- app/src/main/java/com/pippop/managers/BlowoutManager.java
+++ app/src/main/java/com/pippop/managers/BlowoutManager.java
@@ -9,7 +9,6 @@ public class BlowoutManager {
   private static final double PERIL_THRESHOLD = .8;
 
   private boolean isGameOver = false;
-
   private double flash;
   private Color outlineColor = Color.WHITE;
 
@@ -21,14 +20,14 @@ public class BlowoutManager {
     double percentageFilled = getPercentageFilled(graph);
     if (percentageFilled > 1) {
       isGameOver = true;
+      return;
+    }
+    if (percentageFilled < PERIL_THRESHOLD) {
+      outlineColor = Color.WHITE;
     } else {
-      if (percentageFilled < PERIL_THRESHOLD) {
-        outlineColor = Color.WHITE;
-      } else {
-        flash += (delta + 200 * percentageFilled) / 300;
-        double whiteness = 1 - percentageFilled * (1 + Math.sin(flash)) / 2;
-        outlineColor = new Color(1f, (float) whiteness, (float) whiteness, 1f);
-      }
+      flash += (delta + 200 * percentageFilled) / 300;
+      double whiteness = 1 - percentageFilled * (1 + Math.sin(flash)) / 2;
+      outlineColor = new Color(1f, (float) whiteness, (float) whiteness, 1f);
     }
   }
 
