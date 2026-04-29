9d0a4c988ed9af57e671d0a7a35b387f63f82165
Tommaso Sciortino <sciortino@gmail.com>
2026-04-28 20:35:50 -0700

    Refactor main loop into modular helper functions for camera and graph dumping

--- rust_core/src/main.rs
+++ rust_core/src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::*;
+use rust_core::graph::Graph;
 use rust_core::managers::game::{GameController, Interaction};
 use rust_core::resources::Resources;
 
@@ -14,42 +15,31 @@ async fn main() {
     let mut controller = GameController::new(&resources);
 
     loop {
-        let dt = get_frame_time();
-
-        let aspect = screen_width() / screen_height();
-        let camera = Camera2D {
-            target: vec2(0.0, 0.0),
-            zoom: vec2(1.0 / 300.0, aspect / 300.0),
-            ..Default::default()
-        };
-
-        // Input handling
-        let interaction = get_interaction(&camera);
-        controller.handle_input(interaction);
-
+        let camera = get_camera();
+        controller.handle_input(get_interaction(&camera));
+        controller.update(get_frame_time());
+        controller.draw(&camera);
         if is_key_pressed(KeyCode::D) {
-            let dump = controller.graph.dump_state();
-            #[cfg(not(target_arch = "wasm32"))]
-            {
-                use std::io::Write;
-                if let Ok(mut file) = std::fs::File::create("graph_dump.txt") {
-                    let _ = file.write_all(dump.as_bytes());
-                    log::info!("Graph state dumped to graph_dump.txt");
-                }
-            }
-            #[cfg(target_arch = "wasm32")]
-            {
-                log::info!("{}", dump);
-            }
+            dump_graph(&controller.graph);
         }
 
-        // Game Logic Update (includes physics and animation)
-        controller.update(dt);
-
-        // Rendering
-        controller.draw(&camera);
-
         next_frame().await
+    }
+}
+
+fn dump_graph(graph: &Graph) {
+    let dump = graph.dump_state();
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        use std::io::Write;
+        if let Ok(mut file) = std::fs::File::create("graph_dump.txt") {
+            let _ = file.write_all(dump.as_bytes());
+            log::info!("Graph state dumped to graph_dump.txt");
+        }
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        log::info!("{}", dump);
     }
 }
 
@@ -68,5 +58,14 @@ fn get_interaction(camera: &Camera2D) -> Option<Interaction> {
         })
     } else {
         None
+    }
+}
+
+fn get_camera() -> Camera2D {
+    let aspect = screen_width() / screen_height();
+    Camera2D {
+        target: vec2(0.0, 0.0),
+        zoom: vec2(1.0 / 300.0, aspect / 300.0),
+        ..Default::default()
     }
 }
